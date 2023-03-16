635781ffe43c9af3b51137c7365485b169d35eb2
CesarCent <centurion_cesar1234@hotmail.com>
2023-03-16 12:43:37 -0500

    DB_USER root

--- php/mysqli_conexion.php
+++ php/mysqli_conexion.php
@@ -1,6 +1,6 @@
 <?php 
-    define('DB_USER', 'CesarCenturion');
-    define('DB_PASSWORD', 'vU-.mCRg]dFMf!!Y');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'inventorioistg');
 
