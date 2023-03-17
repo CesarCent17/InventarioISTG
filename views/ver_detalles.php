3d50f9ef998cb52ed65cca3853e80ec243ec9cd9
CesarCent <centurion_cesar1234@hotmail.com>
2023-03-16 22:22:18 -0500

    Vista ver detalles estatica

---
+++ views/ver_detalles.php
@@ -0,0 +1,285 @@
+<?php
+    require('../php/mysqli_conexion.php');
+    require('../php/utils_query.php');
+    session_start();
+	 // Verificamos que el usuario esté iniciado sesión
+    if(!isset($_SESSION['usuario'])) {
+        // Si el usuario no está iniciado sesión, lo redirigimos a la página de inicio de sesión
+        header("Location: ../views/login.php");
+    } else {
+		$usuario = $_SESSION['usuario'];
+    	echo "<script> console.log(" . json_encode($usuario) . "); </script>";
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Ver Detalles | ISTG</title>
+	<link rel="stylesheet" href="../css/normalize.css">
+	<link rel="stylesheet" href="../css/sweetalert2.css">
+	<link rel="stylesheet" href="../css/material.min.css">
+	<link rel="stylesheet" href="../css/material-design-iconic-font.min.css">
+	<link rel="stylesheet" href="../css/jquery.mCustomScrollbar.css">
+	<link rel="stylesheet" href="../css/main.css">
+	<link rel="stylesheet" href="../css/style.css">
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="../js/jquery-1.11.2.min.js"><\/script>')</script>
+	<script src="../js/material.min.js" ></script>
+	<script src="../js/sweetalert2.min.js" ></script>
+	<script src="../js/jquery.mCustomScrollbar.concat.min.js" ></script>
+	<script src="../js/main.js" ></script>
+	<script src="../js/mostrarInputNuevoCodigo.js" ></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+</head>
+<body>
+
+<!-- navBar -->
+<div class="full-width navBar">
+		<div class="full-width navBar-options">
+			<div class="mdl-tooltip" for="btn-menu">Menu</div>
+			<nav class="navBar-options-list">
+				<ul class="list-unstyle">
+					
+					<li class="text-condensedLight noLink" ><small>ISTG</small></li>
+					<li class="noLink">
+						<figure>
+							<img src="../assets/img/avatar-male.png" alt="Avatar" class="img-responsive">
+						</figure>
+					</li>
+
+					<li class="noLink">
+					<form action="../php/logout.php" method="post">
+						<button type="submit" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">Cerrar sesión</button>
+					</form>
+					</li>
+
+				</ul>
+			</nav>
+		</div>
+	</div>
+
+	
+
+	<!-- navLateral -->
+	<section class="full-width navLateral">
+		<div class="full-width navLateral-bg btn-menu"></div>
+		<div class="full-width navLateral-body">
+			<div class="full-width navLateral-body-logo text-center tittles">
+				<i class="zmdi zmdi-close btn-menu"></i> Inventario 
+			</div>
+			<figure class="full-width" style="height: 77px;">
+				<!-- <div class="navLateral-body-cl">
+					<img src="assets/img/avatar-male.png" alt="Avatar" class="img-responsive">
+				</div> -->
+				<figcaption class="navLateral-body-cr hide-on-tablet">
+					<span style="margin-left: 60px;">
+						<b>Instituto Superior Tecnológico Guayaquil</b><br>
+						<?php
+							$nombrecompleto = $_SESSION['usuario']['nombre']." ".$_SESSION['usuario']['apellido'];
+							$rol = $_SESSION['rol'];
+							echo "<small>".$nombrecompleto."</small><br>";
+							echo '<small>'. $rol . '</small>';
+
+						?>
+					</span>
+				</figcaption>
+			</figure>
+			<div class="full-width tittles navLateral-body-tittle-menu">
+				<i class="zmdi zmdi-desktop-mac"></i><span class="hide-on-tablet">&nbsp; AGREGAR BIEN</span>
+			</div>
+			<nav class="full-width">
+				<ul class="full-width list-unstyle menu-principal">
+
+					<!-- INICIO -->
+					<li class="full-width">
+						<a href="../home.php" class="full-width">
+							<div class="navLateral-body-cl">
+								<i class="zmdi zmdi-home"></i>
+							</div>
+							<div class="navLateral-body-cr hide-on-tablet">
+								INICIO
+							</div>
+						</a>
+					</li>
+
+					<li class="full-width divider-menu-h"></li>
+					<li class="full-width">
+						<a href="bien.php" class="full-width">
+							<div class="navLateral-body-cl">
+								<i class="zmdi zmdi-plus"></i>
+							</div>
+							<div class="navLateral-body-cr hide-on-tablet">
+								AGREGAR BIEN
+							</div>
+						</a>
+					</li>
+					<li class="full-width divider-menu-h"></li>
+					<li class="full-width">
+						<a href="inventario.php" class="full-width">
+							<div class="navLateral-body-cl">
+								<i class="zmdi zmdi-folder"></i>
+							</div>
+							
+							<div class="navLateral-body-cr hide-on-tablet">
+								INVENTARIO
+							</div>
+						</a>
+					</li>
+					<?php
+						$rol = $_SESSION['rol'];
+						if($rol == 'Administrador'){
+							$html = '<li class="full-width divider-menu-h"></li>
+         								<li class="full-width">
+											<a href="usuarios.php" class="full-width">
+												<div class="navLateral-body-cl">
+												<i class="zmdi zmdi-account"></i>
+												</div>
+												<div class="navLateral-body-cr hide-on-tablet">
+												USUARIOS
+												</div>
+											</a>
+									</li>';
+							echo $html;
+						}	
+					?>
+				</ul>
+			</nav>
+		</div>
+	</section>
+
+	<section class="full-width pageContent">
+	<div style="text-align: center; margin-top: 20px" >
+		<h5 class="mdl-color-text--primary">Ver Detalles</h5>
+  </div>
+
+	<!-- Form -->
+      <form class="mdl-grid" action="../php/save_test.php" method="post" style="max-width: 800px; margin: 0 auto;">
+
+          <div class="mdl-cell mdl-cell--12-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-disabled">
+              <input class="mdl-textfield__input" type="text" id="nombre" name="nombre" required value="PRUEBA" readonly>
+              <label class="mdl-textfield__label" for="nombre">Nombre General</label>
+              <span class="mdl-textfield__error">Este campo es requerido</span>
+            </div>
+          </div>
+
+          <div class="mdl-cell mdl-cell--12-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-disabled" >
+              <input class="mdl-textfield__input" type="text" id="descripcion" name="descripcion" required value="PRUEBA" readonly>
+              <label class="mdl-textfield__label" for="descripcion">Descripción</label>
+              <span class="mdl-textfield__error">Este campo es requerido</span>
+            </div>
+          </div>
+
+        
+          <div class="mdl-cell mdl-cell--6-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-disabled">
+              <input class="mdl-textfield__input" type="number" id="numero_de_acta" name="numero_de_acta" value="5" readonly>
+              <label class="mdl-textfield__label" for="numero_de_acta"># Acta</label>
+            </div>
+          </div>
+
+          <div class="mdl-cell mdl-cell--6-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+              <input class="mdl-textfield__input" type="number" id="anio" name="anio" value="2020" readonly>
+              <label class="mdl-textfield__label" for="anio">Año</label>
+            </div>
+          </div>
+
+          <div class="mdl-cell mdl-cell--6-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-disabled">
+              <input class="mdl-textfield__input" type="text" id="campus" name="campus" value="CAMPUS PRUEBA" readonly>
+              <label class="mdl-textfield__label" for="campus">Campus</label>
+            </div>
+          </div>
+
+        <div class="mdl-cell mdl-cell--6-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-disabled">
+              <input class="mdl-textfield__input" type="text" id="area_de_ubicacion" name="area_de_ubicacion" value="BIBLIOTECA" readonly>
+              <label class="mdl-textfield__label" for="area_de_ubicacion">Área de ubicación</label>
+            </div>
+        </div>
+
+
+        <div class="mdl-cell mdl-cell--12-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+              <input class="mdl-textfield__input" type="text" id="codigoISTG" name="codigoISTG" required value="PRUEBA" readonly>
+              <label class="mdl-textfield__label" for="codigo">Código ISTG</label>
+              <span class="mdl-textfield__error">Este campo es requerido</span>
+            </div>
+          
+          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label  div-codigoSENESCYT/SECAP/COLEGIO" id=divCodigo>
+              <input class="mdl-textfield__input" type="text" id="codigoSENESCYT/SECAP/COLEGIO" name="codigoSENESCYT/SECAP/COLEGIO" value="PRUEBA" readonly>
+              <label class="mdl-textfield__label" for="codigo">Código SENESCYT/SECAP/COLEGIO </label>
+          </div>
+        </div>
+
+          <div class="mdl-cell mdl-cell--6-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-disabled">
+              <input class="mdl-textfield__input" type="text" id="origen_del_bien" name="origen_del_bien" value="ORIGEN PRUEBA" readonly>
+              <label class="mdl-textfield__label" for="origen_del_bien">Origen del bien</label>
+            </div>
+          </div>
+
+
+          <div class="mdl-cell mdl-cell--6-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-disabled">
+              <input class="mdl-textfield__input" type="text" id="custodio" name="custodio" value="CUSTODIO PRUEBA" readonly>
+              <label class="mdl-textfield__label" for="custodio">Custodio</label>
+            </div>
+          </div>
+
+
+          <div class="mdl-cell mdl-cell--6-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-disabled">
+              <input class="mdl-textfield__input" type="text" id="proceso_de_adquisicion" name="proceso_de_adquisicion" value="PROCESO PRUEBA" readonly>
+              <label class="mdl-textfield__label" for="proceso_de_adquisicion">Proceso de adquisición</label>
+            </div>
+          </div>
+
+          <div class="mdl-cell mdl-cell--6-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-disabled">
+              <input class="mdl-textfield__input" type="text" id="estado_de_uso" name="estado_de_uso" value="ESTADO PRUEBA" readonly>
+              <label class="mdl-textfield__label" for="estado_de_uso">Estado de uso</label>
+            </div>
+          </div>
+
+          <div class="mdl-cell mdl-cell--6-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-disabled">
+              <input class="mdl-textfield__input" type="text" id="estado_fisico" name="estado_fisico" value="ESTADO PRUEBA" readonly>
+              <label class="mdl-textfield__label" for="estado_fisico">>Estado físico</label>
+            </div>
+          </div>
+
+
+          <div class="mdl-cell mdl-cell--6-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-disabled">
+              <input class="mdl-textfield__input" type="text" id="acta_de_donacion" name="acta_de_donacion" value="SI" readonly>
+              <label class="mdl-textfield__label" for="acta_de_donacion">Acta de donación</label>
+            </div>
+          </div>
+
+          <div class="mdl-cell mdl-cell--12-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+              <textarea class="mdl-textfield__input" type="text" rows= "3" id="observaciones" name="observaciones" readonly>Texto predeterminado</textarea>
+              <label class="mdl-textfield__label" for="observaciones">Observaciones</label>
+            </div>
+          </div>
+        
+        <div class="mdl-cell mdl-cell--12-col">
+            <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--primary" type="submit">
+              Habilitar Edición 
+            </button>
+        </div>
+
+    </form>
+	</section>
+
+	
+</body>
+</html>
