a8ae157e11e43d8eaccb41ae8bfdfbf5bc9ac2ab
CesarCent <centurion_cesar1234@hotmail.com>
2023-03-24 15:13:19 -0500

    Funcionalidad de registrar docentes

---
+++ php/guardar_docente.php
@@ -0,0 +1,22 @@
+<?php
+require('mysqli_conexion.php');
+require('querys_docente.php');
+session_start();
+// Verificamos que el usuario esté iniciado sesión
+if(!isset($_SESSION['usuario'])) {
+   // Si el usuario no está iniciado sesión, lo redirigimos a la página de inicio de sesión
+   header("Location: login.php");
+} else {
+   //Si el usuario tiene una sesion y esta utilizando el metodo POST
+   if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+    
+    // Requeridos
+    $cedula = trim($_POST['cedula']);
+    $nombre = trim($_POST['nombre']);
+    $apellido = trim($_POST['apellido']);
+    $nombre = strtoupper($nombre);
+    $apellido = strtoupper($apellido);
+    insert_docente($conexion, $cedula, $nombre, $apellido);
+   }
+}
+?>
