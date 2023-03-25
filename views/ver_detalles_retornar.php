97e0a77fae011ff84ef60d949baf2ed0a7bc516d
CesarCent <centurion_cesar1234@hotmail.com>
2023-03-25 10:23:53 -0500

    Enviar a Inventario

--- views/ver_detalles_retornar.php
+++ views/ver_detalles_retornar.php
@@ -425,7 +425,7 @@
         
         <div class="mdl-cell mdl-cell--12-col">
             <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--primary" type="submit">
-              Retornar a Inventario
+              Enviar a Inventario
             </button>
         </div>
     </form>
