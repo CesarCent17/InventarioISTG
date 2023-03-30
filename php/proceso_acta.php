65fc9a941645393e5f35126eb00b21ec16706378
CesarCent <centurion_cesar1234@hotmail.com>
2023-03-30 14:55:42 -0500

    Proceso acta sin retorno

--- php/proceso_acta.php
+++ php/proceso_acta.php
@@ -1,14 +1,46 @@
 <?php
-    require('mysqli_conexion.php');
+require('mysqli_conexion.php');
+require('querys_ver_detalle.php');
+require('querys_inventario.php');
 
-    session_start();
-    if(!isset($_SESSION['usuario'])) {
-        // Si el usuario no está iniciado sesión, lo redirigimos a la página de inicio de sesión
-        header("Location: ../views/login.php");
-    } else {
-        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $id_prod = htmlspecialchars(trim($_POST['id_prod']));
-            header("Location: report/actas/generar_actas.php?id=".urlencode($id_prod));
-        }
-    }
+session_start();
+if(!isset($_SESSION['usuario'])) {
+    // Si el usuario no está iniciado sesión, lo redirigimos a la página de inicio de sesión
+    header("Location: ../views/login.php");
+} else {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $id_prod = htmlspecialchars(trim($_POST['id_prod']));
+        $prod = obtener_producto_por_id($conexion, $id_prod);
+        $array_codigo_de_producto = obtener_array_codigo_de_producto($conexion, $id_prod);
+
+        $nombre = $prod['nombre'];
+        $descripcion = $prod['descripcion'];
+        $observaciones = $prod['observaciones'];
+        $campus = obtener_campus_por_id($conexion, $id_prod);
+        $estado_fisico = obtener_estado_fisico($conexion, $id_prod);
+        $area_de_ubicacion = obtener_ubicacion_por_id($conexion, $id_prod);
+        $codigoISTG = $array_codigo_de_producto[0]['codigo'];
+
+        $bien = array(
+            'nombre' => $nombre,
+            'descripcion' => $descripcion,
+            'observaciones' => $observaciones,
+            'campus' => $campus,
+            'estado_fisico' => $estado_fisico,
+            'area_de_ubicacion' => $area_de_ubicacion,
+            'codigoISTG' => $codigoISTG
+        );
 ?>
+<form id="form-bien" action="report/actas/acta.php" method="post">
+  <input type="hidden" name="bien" value="<?php echo htmlspecialchars(serialize($bien)); ?>">
+</form>
+<script>
+  document.getElementById('form-bien').submit();
+</script>
+<?php
+    }
+   
+}
+
+?>
+
--- php/report/actas/generar_actas.php
+++ php/report/actas/generar_actas.php
@@ -1,8 +1,10 @@
 <?php
-require '../vendor/autoload.php';
+    
+    require('../../mysqli_conexion.php');
 
+   
 
-
+require ('../vendor/autoload.php');
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
@@ -10,37 +12,43 @@ use PhpOffice\PhpSpreadsheet\Spreadsheet;
 $archivo = "plantilla_actas.xlsx";
 $documento = IOFactory::load($archivo);
 
+// $id_prod = htmlspecialchars(trim($_GET['id']));
+// $prod = obtener_producto_por_id($conexion, $id_prod);
 
-// Seleccionar la hoja "ACTA"
-$hoja_acta_entrega = $documento->getSheetByName('ACTA ENTREGA');
+ // Seleccionar la hoja "ACTA"
+ $hoja_acta_entrega = $documento->getSheetByName('ACTA ENTREGA');
 
-// Escribir valores en las celdas
-$hoja_acta_entrega->setCellValue('C15', 'Valor C15'); // Codigo ISTG
-
-$hoja_acta_entrega->setCellValue('F15', 'Valor F15'); // Estado Fisico
-$hoja_acta_entrega->setCellValue('G15', 'Valor G15'); // Campus
-$hoja_acta_entrega->setCellValue('H15', 'Valor H15'); // Area de Ubicacion
-
-
-$hoja_acta_donacion = $documento->getSheetByName('ACTA DONACION');
-$hoja_acta_donacion->setCellValue('B13', 'DATO QUEMADO');
-
-$hoja_acta_donacion->setCellValue('E13', 'DATO QUEMADO');
-$hoja_acta_donacion->setCellValue('F13', 'DATO QUEMADO');
-$hoja_acta_donacion->setCellValue('G13', 'DATO QUEMADO');
+ // Escribir valores en las celdas
+ $hoja_acta_entrega->setCellValue('C15', 'Valor C15'); // Codigo ISTG
+ 
+ $hoja_acta_entrega->setCellValue('F15', 'Valor F15'); // Estado Fisico
+ $hoja_acta_entrega->setCellValue('G15', 'Valor G15'); // Campus
+ $hoja_acta_entrega->setCellValue('H15', 'Valor H15'); // Area de Ubicacion
 
 
+ // Seleccionar la hoja "ACTA DONACION"
+ $hoja_acta_donacion = $documento->getSheetByName('ACTA DONACION');
 
-// Guardar el archivo modificado en memoria
-$writer = IOFactory::createWriter($documento, 'Xlsx');
-ob_start(); // Iniciar el buffer de salida
-$writer->save('php://output'); // Guardar el archivo en memoria
+ // Escribir valores en las celdas
+ $hoja_acta_donacion->setCellValue('B13', 'DATO QUEMADO');
+ 
+ $hoja_acta_donacion->setCellValue('E13', 'DATO QUEMADO');
+ $hoja_acta_donacion->setCellValue('F13', 'DATO QUEMADO');
+ $hoja_acta_donacion->setCellValue('G13', 'DATO QUEMADO');
 
-// Establecer las cabeceras HTTP para descargar el archivo
-header('Content-Type: application/vnd.ms-excel');
-header('Content-Disposition: attachment;filename="actas.xlsx"');
-header('Cache-Control: max-age=0');
 
-// Imprimir el contenido del archivo
-echo ob_get_clean();
+ // Guardar el archivo modificado en memoria
+ $writer = IOFactory::createWriter($documento, 'Xlsx');
+ ob_start(); // Iniciar el buffer de salida
+ $writer->save('php://output'); // Guardar el archivo en memoria
+
+ // Establecer las cabeceras HTTP para descargar el archivo
+ header('Content-Type: application/vnd.ms-excel');
+ header('Content-Disposition: attachment;filename="actas.xlsx"');
+ header('Cache-Control: max-age=0');
+
+ // Imprimir el contenido del archivo
+ echo ob_get_clean();
+
+
 ?>
