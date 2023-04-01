c0c3317c16ef1f316cc993783348130fe87010d0
CesarCent <centurion_cesar1234@hotmail.com>
2023-04-01 18:20:09 -0500

    Modularizacion del nav mostrado al administrador

--- views/campus.php
+++ views/campus.php
@@ -107,69 +107,7 @@
 						require('nav_lateral_opciones.php');
 						$rol = $_SESSION['rol'];
 						if($rol == 'Administrador'){
-							$html = '
-									<li class="full-width divider-menu-h"></li>
-										<li class="full-width">
-										<a href="bienes_descartados.php" class="full-width">
-										<div class="navLateral-body-cl">
-										<i class="zmdi zmdi-tag-close"></i>
-										</div>
-										<div class="navLateral-body-cr hide-on-tablet">
-										BIENES DESCARTADOS
-										</div>
-										</a>
-									</li>
-							
-							
-							
-									<li class="full-width divider-menu-h"></li>
-         								<li class="full-width">
-											<a href="usuarios.php" class="full-width">
-												<div class="navLateral-body-cl">
-												<i class="zmdi zmdi-account"></i>
-												</div>
-												<div class="navLateral-body-cr hide-on-tablet">
-												USUARIOS
-												</div>
-											</a>
-										</li>
-										
-										<li class="full-width divider-menu-h"></li>
-         								<li class="full-width">
-											<a href="campus.php" class="full-width">
-												<div class="navLateral-body-cl">
-												<i class="zmdi zmdi-balance"></i>
-												</div>
-												<div class="navLateral-body-cr hide-on-tablet">
-												CAMPUS
-												</div>
-											</a>
-										</li>
-										
-										<li class="full-width divider-menu-h"></li>
-         								<li class="full-width">
-											<a href="area_de_ubicacion.php" class="full-width">
-												<div class="navLateral-body-cl">
-												<i class="zmdi zmdi-pin"></i>
-												</div>
-												<div class="navLateral-body-cr hide-on-tablet">
-												ÁREA DE UBICACIÓN
-												</div>
-											</a>
-										</li>
-										
-										<li class="full-width divider-menu-h"></li>
-         								<li class="full-width">
-											<a href="docentes.php" class="full-width">
-												<div class="navLateral-body-cl">
-												<i class="zmdi zmdi-account"></i>
-												</div>
-												<div class="navLateral-body-cr hide-on-tablet">
-												DOCENTES
-												</div>
-											</a>
-										</li>';
-							echo $html;
+							require('nav_lateral_admin.php');
 						}	
 					?>
 				</ul>
