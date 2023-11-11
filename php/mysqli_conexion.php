94ea9383187e38fe1c4f732014714d8c2074abfc
César Centurión <cesarcent17@outlook.com>
2023-11-11 07:06:31 -0500

    Docker config finished

--- php/mysqli_conexion.php
+++ php/mysqli_conexion.php
@@ -1,7 +1,7 @@
 <?php 
-    define('DB_USER', 'root');
+    define('DB_USER', 'cesarcent');
     define('DB_PASSWORD', 'uZD7t0J66');
-    define('DB_HOST', 'db:3310');
+    define('DB_HOST', 'db');
     define('DB_NAME', 'inventorioistg');
 
     $conexion = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
